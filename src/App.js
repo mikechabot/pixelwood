c02476761958c75321d132876489fcaad0c254b9
Mike Chabot <mike.chabot@live.com>
2018-12-14 22:50:56 -0500

    Remove prev project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
-import Background from "./Background";
-import Body from "./components/Body";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <div className="is-fullwidth is-fullheight">
-      <Background />
-      <Body />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
